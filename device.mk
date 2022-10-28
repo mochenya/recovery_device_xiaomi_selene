1573fa733bfe3abd4d33aa73fccc68cd3e3028ae
LuK1337 <priv.luk@gmail.com>
2022-10-28 19:49:22 +0100

    selene: Enable PRODUCT_USE_DYNAMIC_PARTITIONS
    Change-Id: Ia397b6462c8b1f0c209f9dee18bdc7ccb0228683

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_TARGET_VNDK_VERSION := 31
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
