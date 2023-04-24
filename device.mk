355e7877d2c627d80cb6bb11a2979494a9d4c3b3
bengris32 <bengris32@protonmail.ch>
2023-04-24 10:20:20 +0530

    selene: Enable Virtual A/B OTAs

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
@@ -20,7 +24,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+    bootctrl \
+    update_engine_client
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6768 \
